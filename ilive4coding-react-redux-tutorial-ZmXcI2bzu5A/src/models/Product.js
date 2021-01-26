9bd5b6478ec5c4ff43a0f54d3b48daebdde256ad
Gaurisunil <62568493+Gaurisunil@users.noreply.github.com>
2021-01-26 10:32:20 +0530

    Add files via upload

---
+++ ilive4coding-react-redux-tutorial-ZmXcI2bzu5A/src/models/Product.js
@@ -0,0 +1,10 @@
+class Product {
+  constructor(id, title, image, cost) {
+    this.id = id;
+    this.title = title;
+    this.image = image
+    this.cost = cost;
+  }
+}
+
+export default Product;
